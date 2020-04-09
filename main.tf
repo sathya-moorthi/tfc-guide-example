0e0fbb61132942c5e2c524f313f933211713fa04
sathya_moorthi@yhaoo.com <sathya_moorthi@yhaoo.com>
2020-04-09 09:24:26 +0000

    Thu Apr 09 2020 - Committed from Codeherent platform

--- main.tf
+++ main.tf
@@ -1,22 +1,32 @@
 provider "aws" {
   version = "2.33.0"
-
-  region = var.aws_region
+  region  = var.aws_region
 }
 
 resource "aws_dynamodb_table" "tfc_example_table" {
-  name = var.db_table_name
-
+  name           = var.db_table_name
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
   hash_key       = "UUID"
-
   attribute {
     name = "UUID"
     type = "S"
   }
-
   tags = {
     user_name = var.tag_user_name
   }
 }
+
+provider "aws" {
+  region = ap-southeast-1
+}
+
+terraform {}
+
+provider "aws" {}
+
+provider "aws" {}
+
+provider "aws" {}
+
+provider "aws" {}
